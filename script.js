d4dfdefbbd846daff6d4dd1ef8ece8cf7cf47a1b
nicholasau <89756208+nicholasau@users.noreply.github.com>
2026-06-16 07:07:28 +0800

    Update script.js

--- script.js
+++ script.js
@@ -9,3 +9,12 @@ window.addEventListener('load', () => {
     window.instgrm.Embeds.process();
   }
 });
+
+const menuToggle = document.querySelector('.menu-toggle');
+const navLinks = document.querySelector('.nav-links');
+
+if (menuToggle) {
+  menuToggle.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+  });
+}
